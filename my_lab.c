01844b23a117fe53c8cdbd11f1249ec89c1f7ccf
SirLonc <72229702+SirLonc@users.noreply.github.com>
2022-05-04 19:53:07 +0300

    Update my_lab.c

--- my_lab.c
+++ my_lab.c
@@ -1,10 +1,21 @@
 #include "my_lab.h"
 
 
-
 int bin_len(number) {
     int i = 0;
     int max = 0;
+    int count = 0;
+
+
+    for (int i = 6; i >= 0; i--) {
+        printf("%d", (number >> i) & 1);
+
+        if ((((number >> i) & 1) == 1) && (((number >> (i - 1)) & 1) == 1) && (((number >> (i - 2)) & 1) == 0)) {
+            count++;
+        }
+    }
+    printf("\namount of rows of ones(>1) - %d", count);
+
     while (number != 0)
     {
         if (number % 2) 
